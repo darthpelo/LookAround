39e90ce6fcc4814cd9d7a5bd329c471e709ff65a
Alessio Roberto <darthpelo@gmail.com>
2019-07-04 22:52:26 +0200

    Impreve API functions

--- LookAround/LookAround/Services/FoursquareManager.swift
+++ LookAround/LookAround/Services/FoursquareManager.swift
@@ -19,6 +19,11 @@ protocol FoursquareClientable {
     func getDetail(venueID: String, completion: @escaping (VenueDetail?) -> Void)
 }
 
+enum Query {
+    case search
+    case detail
+}
+
 struct FoursquareManager: FoursquareClientable {
     private var apiClient: FoursquareAPIClient?
     private let getVenuesPath = "venues/search"
@@ -32,25 +37,12 @@ struct FoursquareManager: FoursquareClientable {
         apiClient?.request(path: getVenuesPath, parameter: parameter) { result in
             switch result {
             case let .success(data):
-                let decoder: JSONDecoder = JSONDecoder()
-                do {
-                    let response = try decoder.decode(Response<SearchResponse>.self, from: data)
-                    completion(response.response.venues)
-                } catch {
-                    print(error)
-                    completion(nil)
-                }
+                self.decodeResponse(data, .search, completion: { reponse in
+                    let resp = reponse as? Response<SearchResponse>
+                    completion(resp?.response.venues)
+                })
             case let .failure(error):
-                // Error handling
-                switch error {
-                case let .connectionError(connectionError):
-                    print(connectionError)
-                case let .responseParseError(responseParseError):
-                    print(responseParseError) // e.g. JSON text did not start with array or object and option to allow fragments not set.
-                case let .apiError(apiError):
-                    print(apiError.errorType) // e.g. endpoint_error
-                    print(apiError.errorDetail) // e.g. The requested path does not exist.
-                }
+                self.errorHandler(error)
 
                 completion(nil)
             }
@@ -61,28 +53,44 @@ struct FoursquareManager: FoursquareClientable {
         apiClient?.request(path: "venues/\(venueID)", parameter: [:], completion: { result in
             switch result {
             case let .success(data):
-                let decoder: JSONDecoder = JSONDecoder()
-                do {
-                    let response = try decoder.decode(Response<DetailResponse>.self, from: data)
-                    completion(response.response.venue)
-                } catch {
-                    print(error)
-                    completion(nil)
-                }
+                self.decodeResponse(data, .detail, completion: { reponse in
+                    let resp = reponse as? Response<DetailResponse>
+                    completion(resp?.response.venue)
+                })
             case let .failure(error):
-                // Error handling
-                switch error {
-                case let .connectionError(connectionError):
-                    print(connectionError)
-                case let .responseParseError(responseParseError):
-                    print(responseParseError) // e.g. JSON text did not start with array or object and option to allow fragments not set.
-                case let .apiError(apiError):
-                    print(apiError.errorType) // e.g. endpoint_error
-                    print(apiError.errorDetail) // e.g. The requested path does not exist.
-                }
+                self.errorHandler(error)
 
                 completion(nil)
             }
         })
+    }
+
+    private func decodeResponse(_ data: Data, _ query: Query, completion: (Any?) -> Void) {
+        let decoder: JSONDecoder = JSONDecoder()
+        do {
+            if query == .search {
+                let response = try decoder.decode(Response<SearchResponse>.self,
+                                                  from: data)
+                            completion(response)
+            } else {
+                let response = try decoder.decode(Response<DetailResponse>.self, from: data)
+                completion(response)
+            }
+
+        } catch {
+            completion(nil)
+        }
+    }
+
+    private func errorHandler(_ error: FoursquareClientError) {
+        switch error {
+        case let .connectionError(connectionError):
+            print(connectionError)
+        case let .responseParseError(responseParseError):
+            print(responseParseError) // e.g. JSON text did not start with array or object and option to allow fragments not set.
+        case let .apiError(apiError):
+            print(apiError.errorType) // e.g. endpoint_error
+            print(apiError.errorDetail) // e.g. The requested path does not exist.
+        }
     }
 }
